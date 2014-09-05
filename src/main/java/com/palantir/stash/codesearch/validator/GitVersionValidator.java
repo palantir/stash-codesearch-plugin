4ea9a987a2d7250a9a0b6aa34f1f6ab3f657f4a1
Carl Myers <cmyers@palantir.com>
2014-09-04 18:00:30 -0700

    Add detection for git version that is too old

---
+++ src/main/java/com/palantir/stash/codesearch/validator/GitVersionValidator.java
@@ -0,0 +1,97 @@
+package com.palantir.stash.codesearch.validator;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.atlassian.stash.scm.Command;
+import com.atlassian.stash.scm.CommandOutputHandler;
+import com.atlassian.stash.scm.git.GitCommandBuilderFactory;
+import com.atlassian.stash.scm.git.GitScm;
+import com.atlassian.utils.process.ProcessException;
+import com.atlassian.utils.process.Watchdog;
+import com.google.common.io.LineReader;
+
+/**
+ * This component should be instantiated when the plugin is started up, allowing us to fail the plugin activation of the
+ * version of git is detected to be incompatible.
+ * 
+ * @author cmyers
+ * 
+ */
+public class GitVersionValidator {
+
+    private static final Integer[] REQUIRED_GIT_VERSION = { 1, 8, 0, 0 };
+    private static final String REQUIRED_GIT_VERSION_STRING = "1.8.0.0";
+
+    public GitVersionValidator(GitScm gitScm) {
+        System.out.println("Testing Git Version");
+        String gitVersion = validateVersion(gitScm);
+        System.out.println("Determined Git Version OK! (" + gitVersion + ")");
+    }
+
+    private String validateVersion(GitScm gitScm) {
+        GitCommandBuilderFactory gcbf = gitScm.getCommandBuilderFactory();
+        CommandOutputHandler<String> coh = new CommandOutputHandler<String>() {
+
+            private final StringBuilder sb = new StringBuilder();
+            private boolean finished = false;
+
+            @Override
+            public void process(InputStream output) throws ProcessException {
+                final LineReader lr = new LineReader(new InputStreamReader(output));
+                String line = null;
+                try {
+                    while ((line = lr.readLine()) != null) {
+                        sb.append(line);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException("Error while determining git version:", e);
+                }
+            }
+
+            @Override
+            public void complete() throws ProcessException {
+                finished = true;
+            }
+
+            @Override
+            public void setWatchdog(Watchdog watchdog) {
+            }
+
+            @Override
+            public String getOutput() {
+                if (!finished) {
+                    throw new IllegalStateException("Called getOutput() before complete()");
+                }
+                return sb.toString();
+            }
+        };
+        Command<String> versionCommand = gcbf.builder().version(coh);
+        versionCommand.call();
+        String versionString = coh.getOutput();
+        // version string looks like this: git version 1.7.9.5
+        String versionNumber = versionString.split(" ")[2];
+        String[] versionParts = versionNumber.split("\\.");
+
+        int min =
+            (versionParts.length <= REQUIRED_GIT_VERSION.length) ? versionParts.length : REQUIRED_GIT_VERSION.length;
+
+        for (int i = 0; i < min; ++i) {
+            try {
+                int test = Integer.parseInt(versionParts[i]);
+                if (test > REQUIRED_GIT_VERSION[i]) {
+                    return versionNumber;
+                } else if (test < REQUIRED_GIT_VERSION[i]) {
+                    throw new IllegalStateException("Invalid Git Version '" + versionNumber
+                        + "', must be equal to or greater than '" + REQUIRED_GIT_VERSION_STRING + "'");
+                }
+                // else: keep checking next number
+            } catch (NumberFormatException e) {
+                throw new RuntimeException("Error while determining git version:", e);
+            }
+        }
+        // if we get here, we had a perfect match
+        return versionNumber;
+    }
+}
